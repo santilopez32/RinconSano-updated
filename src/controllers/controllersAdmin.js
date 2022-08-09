f23cec43b3bf108640f412906ebb99509ac83742
Nicolaslui <nicolaslui97@gmail.com>
2022-08-08 22:08:04 -0300

    Modificamos las vistas de administrador, agregamos clasificaciones a los productos y se modifico la vista de productos teniendo en cuenta dichas categorias

--- src/controllers/controllersAdmin.js
+++ src/controllers/controllersAdmin.js
@@ -15,7 +15,7 @@ const controllersAdmin = {
         let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
         let ultimoProd = productos.pop();
         productos.push(ultimoProd);
-        console.log(ultimoProd.id +1)
+        console.log(ultimoProd)
         let nuevoProducto = {
             id: ultimoProd.id +1,
             nombre : req.body.nombre,
