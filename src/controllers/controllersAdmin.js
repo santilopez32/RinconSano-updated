de68486d49f6c570a2e9dd5a4999670f347611ab
santilopez32 <santilopez32@gmail.com>
2022-08-27 19:31:02 -0300

    Destroy agregado con exito

--- src/controllers/controllersAdmin.js
+++ src/controllers/controllersAdmin.js
@@ -5,8 +5,6 @@ let db = require("../database/models")
 
 const controllersAdmin = {
 	home: (req, res) => {
-        /*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-		return res.render(path.resolve(__dirname, '../views/admin/administrar'), {productos});*/
         db.Productos.findAll()
             .then(Productos => {
                 res.render(path.resolve(__dirname, '../views/admin/administrar'), {Productos: Productos})
@@ -17,8 +15,6 @@ const controllersAdmin = {
             .then(Categorias => {
                 res.render(path.resolve(__dirname, '../views/admin/create'), {Categorias: Categorias})
             })
-        /*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-		return res.render(path.resolve(__dirname, '../views/admin/create'), {productos});*/
 	},
     save: (req, res) => {
         db.Productos.create({            
@@ -29,48 +25,6 @@ const controllersAdmin = {
             Descuento: req.body.descuento,
             Imagen: req.file.filename
         })
-        /*Habría que ver de agregar las lineas de código de abajo. Es necesario implementar el then*/
-        /*.then(confirmacion => {
-            let response;
-            if (confirmacion) {
-                response = {
-                    info: {
-                        status: 200,
-                        total: confirmacion.length,
-                        url: "api/movies/create"
-                    },
-                    data: confirmacion
-                }
-            }
-            else {
-                response = {
-                    info: {
-                        status: 200,
-                        total: confirmacion.length,
-                        url: "api/movies/create"
-                    },
-                    data: confirmacion
-                }
-            }
-            res.json(response)
-        })*/
-
-        /*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-        let ultimoProd = productos.pop();
-        productos.push(ultimoProd);
-        console.log(ultimoProd)
-        let nuevoProducto = {
-            id: ultimoProd.id +1,
-            nombre : req.body.nombre,
-            descripcion: req.body.descripcion,
-            categoria: req.body.categoria,
-            precio: req.body.precio,
-            descuento: req.body.descuento,
-            imagen: req.file.filename
-        }
-        productos.push(nuevoProducto);
-        let nuevoProductoGuardar = JSON.stringify(productos,null,2);
-        fs.writeFileSync(path.resolve(__dirname,'../database/productos.json'), nuevoProductoGuardar);*/
         res.redirect('/administrar');
 	},
     edit: (req, res) => {
@@ -81,10 +35,6 @@ const controllersAdmin = {
                 return res.render(path.resolve(__dirname, '../views/admin/edit'), {Productos: Productos, Categoria:Categoria})
             })
             .catch(error => res.send(error));
-        /*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-        const prodId = req.params.id;
-        let prodEditar = productos.find(prod=> prod.id == prodId);
-		return res.render(path.resolve(__dirname, '../views/admin/edit'), {prodEditar});*/
 	},
     update: (req, res) => {
         let Productos = {
@@ -96,42 +46,23 @@ const controllersAdmin = {
             Imagen: req.body.imagen
         }
         db.Productos.update(Productos, {where:{idProducto: req.params.id}})
-        res.redirect('/administrar')
-        /*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-        req.body.id = req.params.id;
-        req.body.imagen = req.file ? req.file.filename : req.body.oldImagen;
-        let productosUpdate = productos.map(producto =>{
-            if(producto.id == req.body.id){
-                return producto = req.body;
-            }
-            return producto;
-        })
-        let productosActualizar = JSON.stringify(productosUpdate,null,2);
-        fs.writeFileSync(path.resolve(__dirname,'../database/productos.json'),productosActualizar)
-        res.redirect('/administrar');*/
-        
+        res.redirect('/administrar')        
     },
     show: (req, res) => {
-        db.Productos.findByPk(req.params.id)
-            .then(Productos => {
-                res.render(path.resolve(__dirname, '../views/admin/detail'), {Productos: Productos})
+        let Productos = db.Productos.findByPk(req.params.id, {include: [{association: 'Categoria'}]});
+        let Categoria = db.Categoria.findByPk(req.params.id);
+        Promise.all([Productos, Categoria])
+            .then(([Productos, Categoria]) => {
+                return res.render(path.resolve(__dirname, '../views/admin/detail'), {Productos: Productos, Categoria:Categoria})
             })
-        /*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-        let miProducto;
-        productos.forEach(producto => {
-            if(producto.id == req.params.id){
-                miProducto = producto;
-            }
-        });
-		return res.render(path.resolve(__dirname, '../views/admin/detail'), {miProducto});*/
+            .catch(error => res.send(error));
 	},
     destroy: (req, res) => {
-        let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-        const productoDeleteId = req.params.id;
-        const productosFinal = productos.filter(producto => producto.id != productoDeleteId);
-        let productosGuardar = JSON.stringify(productosFinal,null,2)
-        fs.writeFileSync(path.resolve(__dirname, '../database/productos.json'),productosGuardar);
-        res.redirect('/administrar');
+
+        db.Productos.destroy({where:{idProducto: req.params.id}})
+        res.redirect('/administrar')
+
+        
 	},
     
 }
