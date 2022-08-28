de5ff14213421d05386b23d0bdcbd92d60817a08
santilopez32 <santilopez32@gmail.com>
2022-08-28 09:47:57 -0300

    Hicimos funcionar productos y product detail

--- src/controllers/controllersProduct.js
+++ src/controllers/controllersProduct.js
@@ -6,21 +6,26 @@ let db = require("../database/models")
 
 const controllersProduct = {
 	productDetail: (req, res) => {
-		let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
+		/*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
         let miProd;
         productos.forEach(producto => {
             if(producto.id == req.params.id){
                 miProd = producto;
             }
         });
-        res.render(path.resolve(__dirname, '../views/products/productDetail'), {miProd})
+        res.render(path.resolve(__dirname, '../views/products/productDetail'), {miProd})*/
+        let Productos = db.Productos.findByPk(req.params.id, {include: [{association: 'Categoria'}]});
+        let Categoria = db.Categoria.findByPk(req.params.id);
+        Promise.all([Productos, Categoria])
+            .then(([Productos, Categoria]) => {
+                return res.render(path.resolve(__dirname, '../views/products/productDetail'), {Productos: Productos, Categoria:Categoria})
+            })
+            .catch(error => res.send(error));
 	},
     productCart: (req, res) => {
 		return res.render(path.resolve(__dirname, '../views/products/productCart'));
 	},
     products: (req, res) => {
-		/*let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/productos.json')));
-		return res.render(path.resolve(__dirname, '../views/products/products'), {productos});*/
         db.Productos.findAll()
             .then(Productos => {
                 res.render(path.resolve(__dirname, '../views/products/products'), {Productos: Productos})
