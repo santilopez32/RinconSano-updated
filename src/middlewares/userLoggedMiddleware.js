fac7bcea78d6f2783c62379418591777bef16174
santilopez32 <santilopez32@gmail.com>
2022-09-13 11:28:59 -0300

    Arreglamos el logout

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -3,26 +3,27 @@ let db = require("../database/models")
 
 function userLoggedMiddleware(req, res, next) {
 	res.locals.isLogged = false;
-
-	let emailInCookie = req.cookies.userEmail;
-	/*let userFromCookie = User.findByField('email', emailInCookie);*/
-	
-
-	if (emailInCookie) {
-		db.Usuarios.findOne({ where: { email: emailInCookie}})
-		.then((usuario) => {
-			delete usuario.password
-			req.session.userLogged = usuario;
+	const emailInCookie = req.cookies.userEmail;
+	if(!req.session.userLogged){
+		if(emailInCookie){
+			db.Usuarios.findOne({ where: { email: emailInCookie}})
+				.then((usuario) => {
+					delete usuario.password
+					req.session.userLogged = usuario;
+					res.locals.isLogged = true;
+					res.locals.userLogged = req.session.userLogged;
 		})
-		
-	}
 
-	if (req.session.userLogged) {
+		}
+
+	} else {
 		res.locals.isLogged = true;
 		res.locals.userLogged = req.session.userLogged;
-	}
 
+	}
 	next();
+
+	
 }
 
 module.exports = userLoggedMiddleware;
